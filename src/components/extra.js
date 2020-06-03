d94e39dac00944b91550023d994d7594c8c93ac3
unknown <you@example.com>
2020-06-03 23:50:24 +0600

    payment integrated

--- src/components/extra.js
+++ src/components/extra.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Footer from './Footer';
-import axios from 'axios';
 import { useForm} from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import {Elements} from '@stripe/react-stripe-js';
@@ -8,96 +7,34 @@ import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
 
 const PersonalDetails = () => {
-    const { register,  errors } = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const [orderId, setOrderId] = useState(null);
     const [membershipInfo, setMembershipInfo] = useState(null);
     const {key} = useParams();
     const stripePromise = loadStripe('pk_test_rFGZ5SVmq1jmDLoH9TNKeG5C00gtdXy9q4');
 
-    const [membershipId, setMembershipId] = useState(null) //pass this membership id to payment option
-    const [membershipType, setMembershipType] = useState(null)  //set this taking props from pricing page
 
 
-
-    // const onSubmit = data => { 
-    //     setMembershipInfo(data);
+    const onSubmit = data => { 
+        setMembershipInfo(data);
         
-    //   }
+      }
 
-      // const handlePlaceOrder = (payment) =>{
-      //   const orderDetail = {MonthlyPlan: key, shipment: membershipInfo, payment: payment};
-      //   fetch('',{
-      //     method: 'POST',
-      //     headers:{
-      //         'Content-Type': 'application/json'
-      //     },
-      //     body: JSON.stringify(orderDetail)
-      //   })
-      //   .then(res => res.json())
-      //   .then(order => {
-      //     setOrderId(order._id);
+      const handlePlaceOrder = (payment) =>{
+        const orderDetail = {MonthlyPlan: key, shipment: membershipInfo, payment: payment};
+        fetch('',{
+          method: 'POST',
+          headers:{
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(orderDetail)
+        })
+        .then(res => res.json())
+        .then(order => {
+          setOrderId(order._id);
          
-      //   })
-      // }
-
-    //dont change code below
-    const [ isFormVisible, setIsFormVisible] = useState(true)
-    const [ isPaymentVisible, setIsPaymentVisible] = useState(false)
-    const [ isMsgVisible, setIsMsgVisible] = useState(false)
-    const memberData = {
-        firstName:'',
-        lastName: '',
-        email:'',
-        mobile : '',
-        dob : '',
-        gender:'',
-        address:'',
-        payment:''
-    }
-    const handleChange = (e)=>{
-        memberData[e.target.name] = e.target.value
-    }
-    const handlePayment = (e)=>{
-        e.preventDefault()
-        setIsMsgVisible(true)
-        setIsPaymentVisible(false)
-    }
-
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        setIsFormVisible(false)
-        setIsPaymentVisible(true)
-        submitToDatabase()
-    }
-    
-
-    const submitToDatabase = ()=>{
-        const payLoad ={
-            firstName: memberData.firstName,
-            lastName:  memberData.lastName,
-            email: memberData.email,
-            mobile :  memberData.mobile, 
-            dob :  memberData.dob,
-            gender: memberData.gender,
-            address: memberData.address,
-            membershipType: membershipType,
-            payment: memberData.payment
-        }
-        axios({
-            url:'https://power-gym-server.herokuapp.com/api/save',
-            method:'POST',
-            data: payLoad
-            
         })
-        .then((res)=>{
-            console.log("data has been sent")
-            setMembershipId(res.data.id)
-            
-        })
-        .catch(()=>{
-            console.log("internal server error")
-        })
-    }
+      }
 
     return (
         <div>
@@ -120,10 +57,10 @@ const PersonalDetails = () => {
         </div>
 
         <div className="row">
-        <div style={{display: !memberData }} className="col-md-6">
+        <div style={{display: key }} className="col-md-6">
           <h3>Membership Information</h3>
 
-        <form className="ship-form " onSubmit={handleSubmit}>
+        <form className="ship-form " onSubmit={handleSubmit(onSubmit)}>
                     <input name="firstname" className="form-control"  ref={register({ required: true })} placeholder="First Name"/>
                     {errors.firstname && <span className="error">First Name is required</span>}<br/>
 
@@ -139,31 +76,36 @@ const PersonalDetails = () => {
                     <input name="date" type="date" className="form-control"  ref={register({ required: true })} placeholder="Date of Birth : dd/mm/yy" />
                     {errors.date && <span className="error">Date is required</span>}<br/>
 
-                    <select name="gender" className="form-control" onChange={handleChange} id="cars" ref={register({ required: true })} placeholder="Gender" >
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                    </select>
+                    <input name="gender" className="form-control"  ref={register({ required: true })} placeholder="Gender" />
                     {errors.gender && <span className="error">Gender is required</span>}<br/>
 
                     <input name="address" className="form-control"  ref={register({ required: true })} placeholder="Your Address" />
                     {errors.address && <span className="error">Address is required</span>}<br/>
+
+                    <input name="country" className="form-control"  ref={register({ required: true })} placeholder="Country" />
+                    {errors.country && <span className="error">Country is required</span>}<br/>
+
+                    <input name="city" className="form-control"  ref={register({ required: true })} placeholder="City" />
+                    {errors.city && <span className="error">City is required</span>}<br/>
+
+                    <input name="postcode" className="form-control"  ref={register({ required: true })} placeholder="Post Code" />
+                    {errors.postcode && <span className="error">Post Code is required</span>}<br/>
 
                     <button className="btn btn-primary form-control" type="submit" >Next</button>
                 </form> 
                 </div>
 
                 <div  className="col-md-6">
-                    <div style={{marginTop: '200px', display: memberData ? 'block' : 'none'}}>
+                    <div style={{marginTop: '200px', display: membershipInfo ? 'block' : 'none'}}>
                         <h3>Payment Information</h3>
                 <Elements stripe={stripePromise}>
-                    <CheckoutForm handlePlaceOrder={submitToDatabase}></CheckoutForm>
+                    <CheckoutForm handlePlaceOrder={handlePlaceOrder}></CheckoutForm>
                 </Elements>
                 <br/>
                 {
             orderId && <div>
-              <h3>Thank you for joining with us</h3>
-          <p>Your  Id is: {orderId}</p>
+              <h3>Thank you for shopping with us</h3>
+          <p>Your Order Id is: {orderId}</p>
             </div>
           }
           </div>
